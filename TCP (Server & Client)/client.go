51fa4fa815260dfbd743192d3ac03d85a7123564
KotionLotion <jc.melgar.03@gmail.com>
2025-05-06 08:09:41 -0600

    Update client.go

--- TCP (Server & Client)/client.go
+++ TCP (Server & Client)/client.go
@@ -1,48 +1,79 @@
+//go:build client
+
 // Jesse - TCP Client
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "log"
-    "net"
-    "os"
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
-    // connect to server
-    conn, err := net.Dial("tcp", "localhost:8080")
-    if err != nil {
-        log.Fatal("Can't connect: ", err)
-    }
-    defer conn.Close() // close when done
-    fmt.Println("Connected. Type /quit to exit.")
-    
-    // goroutine to handle incoming messages
-    go func() {
-        buf := make([]byte, 1024) // message buffer
-        for {
-            n, err := conn.Read(buf) // read from server
-            if err != nil {
-                log.Println("Server kicked us out")
-                os.Exit(0) // exit if connection stops
-            }
-            fmt.Print(string(buf[:n])) // print message
-        }
-    }()
-    
-    //read user input
-    scanner := bufio.NewScanner(os.Stdin)
-    for scanner.Scan() {
-        msg := scanner.Text() // get user input
-        if msg == "/quit" {
-            fmt.Println("Bye!")
-            return // exit w/ /quit
-        }
-        
-        // send message to server
-        _, err := conn.Write([]byte(msg + "\n"))
-        if err != nil {
-            log.Println("Failed to send:", err)
-            return // exit if fail
-        }}}
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run -tags client client.go <username>")
+		return
+	}
+
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal("Connection failed:", err)
+	}
+	defer conn.Close()
+
+	// Register name
+	_, err = fmt.Fprintf(conn, "NAME:%s\n", os.Args[1])
+	if err != nil {
+		log.Fatal("Name registration failed:", err)
+	}
+
+	// Graceful shutdown
+	quit := make(chan struct{})
+	go func() {
+		sigchan := make(chan os.Signal, 1)
+		signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+		<-sigchan
+		fmt.Println("\nDisconnecting...")
+		fmt.Fprintf(conn, "QUIT\n")
+		close(quit)
+		os.Exit(0)
+	}()
+
+	// Message receiver
+	go func() {
+		scanner := bufio.NewScanner(conn)
+		for scanner.Scan() {
+			select {
+			case <-quit:
+				return
+			default:
+				fmt.Println(scanner.Text())
+			}
+		}
+	}()
+
+	// User input
+	fmt.Printf("Connected as %s. Type messages or QUIT to exit.\n", os.Args[1])
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		select {
+		case <-quit:
+			return
+		default:
+			msg := scanner.Text()
+			if msg == "QUIT" {
+				fmt.Fprintf(conn, "QUIT\n")
+				return
+			}
+			_, err := fmt.Fprintf(conn, "%s\n", msg)
+			if err != nil {
+				log.Println("Send failed:", err)
+				return
+			}
+		}
+	}
+}
